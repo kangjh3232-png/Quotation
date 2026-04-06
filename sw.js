e487d8c05162643698aa52d663436ba366baed7c
kangjh3232-png <kangjh3232@gmail.com>
2026-04-06 14:41:37 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,17 +1,31 @@
-var CACHE='saehan-v3';
-var URLS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
+var CACHE='saehan-v10';
+var BASE='/Quotation';
 self.addEventListener('install',function(e){
-  e.waitUntil(caches.open(CACHE).then(function(c){return c.addAll(URLS);}));
+  e.waitUntil(
+    caches.open(CACHE).then(function(c){
+      return c.addAll([BASE+'/',BASE+'/index.html',BASE+'/manifest.json',BASE+'/icon-192.png',BASE+'/icon-512.png']);
+    })
+  );
   self.skipWaiting();
 });
 self.addEventListener('activate',function(e){
-  e.waitUntil(caches.keys().then(function(ks){
-    return Promise.all(ks.filter(function(k){return k!==CACHE;}).map(function(k){return caches.delete(k);}));
-  }));
+  e.waitUntil(
+    caches.keys().then(function(keys){
+      return Promise.all(keys.filter(function(k){return k!==CACHE;}).map(function(k){return caches.delete(k);}));
+    })
+  );
   self.clients.claim();
 });
 self.addEventListener('fetch',function(e){
-  e.respondWith(caches.match(e.request).then(function(c){
-    return c||fetch(e.request).catch(function(){return caches.match('./index.html');});
-  }));
+  e.respondWith(
+    caches.match(e.request).then(function(c){
+      return c||fetch(e.request).then(function(r){
+        var cl=r.clone();
+        caches.open(CACHE).then(function(ca){ca.put(e.request,cl);});
+        return r;
+      });
+    }).catch(function(){
+      return caches.match(BASE+'/index.html');
+    })
+  );
 });
